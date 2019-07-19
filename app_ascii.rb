d3a0f3b35b8a325588b425efa2821361a435d01f
Mandresy4 <harenamandresy04@gmail.com>
2019-07-19 18:20:50 +0300

    ASCII

---
+++ app_ascii.rb
@@ -0,0 +1,10 @@
+def metho
+print "Saisir ton mot de passe ici (sans chiffre)==> "
+#entrer
+mots = gets.chomp.to_s
+nom = mots.unpack('U*')
+#puts nom
+renverse = nom.collect! {|object| object - 10}
+var = renverse.collect! {|term| term.chr}
+a = var.join
+puts "==> #{a}"
